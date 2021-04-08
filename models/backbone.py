6fe8171fc5f6aec9ed315c309306a4befb89936b
vladimir <chernvld@gmail.com>
2021-04-08 20:54:34 +0300

    zero-initialization added to resnet_backbone

--- models/backbone.py
+++ models/backbone.py
@@ -45,3 +45,43 @@ def resnet_backbone(
     return_layer = {f'layer{returned_layer}': f'layer{returned_layer}'}
 
     return IntermediateLayerGetter(model=backbone, return_layers=return_layer)
+
+
+def resnet_backbone_zero_init(name='resnet50',
+                              pretrained=False,
+                              trainable_layers=3,
+                              returned_layer=4,
+                              **kwargs):
+    if isinstance(pretrained, str):
+        backbone = resnet.__dict__[name](pretrained=False, norm_layer=FrozenBatchNorm2d)
+        state_dict = torch.load(pretrained)
+        # I WANNA KILL MY FAMILY AND MYSELF
+        state_dict.pop('fc.weight')
+        state_dict.pop('fc.bias')
+        backbone.load_state_dict(state_dict, strict=False)
+    else:
+        backbone = resnet.__dict__[name](pretrained=pretrained, norm_layer=FrozenBatchNorm2d)
+
+    assert 0 <= trainable_layers <= 5
+    layers_to_train = ['layer4', 'layer3', 'layer2', 'layer1', 'conv1'][:trainable_layers]
+
+    if trainable_layers == 5:
+        layers_to_train.append('bn1')
+
+    # layer4 zero-initialize
+    for m in backbone.layer4.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.zeros_(m.weight)
+        elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+            nn.init.constant_(m.weight, 1)
+            nn.init.constant_(m.bias, 0)
+
+    # freeze layers
+    for name, parameter in backbone.named_parameters():
+        if all([not name.startswith(layer) for layer in layers_to_train]):
+            parameter.requires_grad_(False)
+
+    assert 0 < returned_layer < 5
+    return_layer = {f'layer{returned_layer}': f'layer{returned_layer}'}
+
+    return IntermediateLayerGetter(model=backbone, return_layers=return_layer)
