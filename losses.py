df3ee158a30fbd0714988b04055a9fe69ed84413
Stas Mikhaylevskiy <m1xst99@yandex.ru>
2021-04-15 17:34:12 +0300

    pos_weight added to BinaryFocalLoss

--- losses.py
+++ losses.py
@@ -2,12 +2,16 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+from typing import Optional
+from torch import Tensor
+
 
 class BinaryFocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=None):
+    def __init__(self, gamma=2, alpha=None, pos_weight: Optional[Tensor] = None):
         super(BinaryFocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
+        self.pos_weight = pos_weight
 
     def forward(self, input, target):
         """
@@ -22,4 +26,6 @@ class BinaryFocalLoss(nn.Module):
         fl = (1 - pt) ** self.gamma * bce
         if self.alpha is not None:
             fl = torch.where(target.bool(), self.alpha * fl, (1 - self.alpha) * fl)
+        if self.pos_weight is not None:
+            fl = torch.where(target.bool(), self.pos_weight * fl, fl)
         return fl.mean()
