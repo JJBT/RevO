7f2344d30df82064de744e92e96a808012c6c7c7
vladimir <chernvld@gmail.com>
2021-05-01 19:03:11 +0300

    logging output directory

--- train.py
+++ train.py
@@ -1,14 +1,17 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 
 def run_train(cfg):
+    logging.info(f'Output directory - {os.getcwd()}')
     from trainer import Trainer
     trainer = Trainer(cfg)
     trainer.run_train()
+    logging.info(f'Output directory - {os.getcwd()}')
 
 
 @hydra.main(config_path='conf', config_name='config')
