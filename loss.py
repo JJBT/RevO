7a53a4754e10578916b0777b96d6c47c1840c6b7
Stas Mikhaylevskiy <m1xst99@yandex.ru>
2021-05-01 18:56:01 +0300

    :)

--- loss.py
+++ loss.py
@@ -1,5 +1,3 @@
-import time
-
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -114,7 +112,7 @@ class FocalEIoULoss(nn.Module):
         return eiou_fl
 
 
-class CustomYOLOLoss(nn.Module):
+class CustomYOLOLoss(torch.nn.Module):
     def __init__(self,
                  bbox_criterion,
                  conf_criterion,
@@ -128,99 +126,6 @@ class CustomYOLOLoss(nn.Module):
         self.lambda_noobj = lambda_noobj
         self.lambda_bbox = lambda_bbox
         self.lambda_obj = lambda_obj
-
-
-    def forward(self, input, target):
-        '''
-        Compute YOLOv3 loss with {G, C, E}IoULoss for bbox predictions
-        ```{input, target}[i, j, k, :] = (c, x, y, w, h)```
-        :param input (torch.Tensor[N, S, S, 5]): raw model output (logits)
-        :param target (torch.Tensor[N, S, S, 5]): yolo target
-        :return:
-        '''
-
-        obj_mask = target[..., 0] > 0.
-        noobj_mask = ~obj_mask
-
-        n_bbox = input.shape[-1] // 5
-        conf_idx = torch.arange(n_bbox * 5) % 5 == 0
-        coord_idx = ~conf_idx
-
-        obj_pred, noobj_pred = input[obj_mask], input[noobj_mask]
-        obj_target, noobj_target = target[obj_mask], target[noobj_mask]
-
-        # Cells with no objects
-        noobj_pred_logit = noobj_pred[..., conf_idx]
-        loss_noobj = self.conf_criterion(noobj_pred_logit, torch.zeros_like(noobj_pred_logit))
-
-        # Cells with objects
-        # Responsible bboxes
-        pred_bbox = torch.sigmoid(
-            obj_pred[..., coord_idx].view(obj_pred.shape[0] * n_bbox, 4)
-        )
-        target_bbox = obj_target[..., 1:]
-        iou = compute_iou(
-            pred_bbox,
-            torch.repeat_interleave(target_bbox[:, None, :], repeats=n_bbox, dim=1).view(obj_target.shape[0] * n_bbox, 4),
-            bbox_transform=xcycwh2xyxy
-        )
-        responsible_idx = iou.view(obj_target.shape[0], n_bbox).argmax(dim=1)
-        pred_resp_bbox = torch.stack(
-            [pred_bbox[n_bbox * i + s] for i, s in zip(range(obj_pred.shape[0]), responsible_idx)]
-        )
-
-        loss_bbox = self.bbox_criterion(
-            pred_resp_bbox,
-            target_bbox
-        )
-
-        # Responsible confs
-        pred_logit = obj_pred[..., conf_idx]
-        pred_resp_logit = torch.stack(
-            [pred_logit[i, s] for i, s in zip(range(obj_pred.shape[0]), responsible_idx)]
-        )
-        loss_obj = self.conf_criterion(
-            pred_resp_logit,
-            torch.ones_like(pred_resp_logit)
-        )
-
-        # Not responsible confs
-        idx = torch.repeat_interleave(torch.arange(n_bbox)[None, :], repeats=obj_pred.shape[0], dim=0).to(responsible_idx.device)
-        not_responsible_idx = idx[idx != responsible_idx.unsqueeze(1)]
-        if not_responsible_idx.nelement() > 0:
-            pred_not_resp_logit = torch.stack(
-                [pred_logit[i, s] for i, s in zip(torch.repeat_interleave(torch.arange(obj_pred.shape[0]), n_bbox - 1), not_responsible_idx)]
-            )
-            loss_noobj += self.conf_criterion(
-                pred_not_resp_logit,
-                torch.zeros_like(pred_not_resp_logit)
-            )
-        loss = self.lambda_obj * loss_obj + self.lambda_bbox * loss_bbox + \
-               self.lambda_noobj * loss_noobj
-
-        return {
-            'loss': loss,
-            'loss_noobj': loss_noobj.detach(),
-            'loss_bbox': loss_bbox.detach(),
-            'loss_obj': loss_obj.detach()
-        }
-
-
-class CustomLoss(torch.nn.Module):
-    def __init__(self,
-                 bbox_criterion,
-                 conf_criterion,
-                 lambda_noobj=1,
-                 lambda_bbox=1,
-                 lambda_obj=1
-                 ):
-        super(CustomLoss, self).__init__()
-        self.bbox_criterion = bbox_criterion
-        self.conf_criterion = conf_criterion
-        self.lambda_noobj = lambda_noobj
-        self.lambda_bbox = lambda_bbox
-        self.lambda_obj = lambda_obj
-
 
     def forward(self, input, target):
         """Compute YOLO loss
