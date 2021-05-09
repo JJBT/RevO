c7040853c232069b82cf7a8244bc2e98f67778f3
vladimir <chernvld@gmail.com>
2021-05-09 22:54:37 +0300

    initialize_fn to transformer

--- models/transformer.py
+++ models/transformer.py
@@ -87,6 +87,8 @@ class SimpleTransformer(nn.Module):
         self.ln_f = nn.LayerNorm(self.embd_dim)
         self.head = nn.Linear(self.embd_dim, self.out_dim, bias=False)
 
+        self.apply(self._init_weights)
+
     def forward(self, input):
         x, mask = input['x'], input['mask']
         x = self.drop(x)
@@ -95,3 +97,12 @@ class SimpleTransformer(nn.Module):
         x = self.head(x)
 
         return x
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
